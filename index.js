2eade5e70e33db1f78f28a4719564dffd7d740a9
alex <alexnasiali45@gmail.com>
2024-10-03 17:19:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove('mainElement');
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const myName = "Alex";
+newHeader.textContent = `${myName}is the champion`;
